c494c4f73d3cf1a39d19868209b762095e2c19c9
Joseph Tessitore <joetgamerman3@gmail.com>
2022-12-22 15:20:07 -0500

    add ufo

--- init.lua
+++ init.lua
@@ -233,9 +233,30 @@ require("lazy").setup({
 			}
 			require("dapui").setup({})
 		end,
+	},
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = {"kevinhwang91/promise-async", "nvim-treesitter/nvim-treesitter"},
+		event = "VeryLazy",
+		config = function()
+			require("ufo").setup({
+				provider_selector = function(bufnr, filetype, buftype)
+					return {"treesitter", "indent"}
+				end
+			})
+		end,
 	}
 })
 
+vim.o.expandtab = true
 vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+vim.o.foldcolumn = '1'
+vim.o.foldenable = true
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.nu = true
+vim.o.shiftwidth = 4
+vim.o.smartindent = true
+vim.o.tabstop = 4
 
 vim.cmd("colorscheme catppuccin")

ceb5965e9c17e0935dfc4a0ff785a3267b35477b
Joseph Tessitore <joetgamerman3@gmail.com>
2022-12-22 14:53:55 -0500

    Add mappings for cmp

--- init.lua
+++ init.lua
@@ -58,16 +58,33 @@ require("lazy").setup({
 			local cmp = require("cmp")
 			require("mason-lspconfig")
 			require("luasnip.loaders.from_vscode").lazy_load()
+			local luasnip = require("luasnip")
 
 			cmp.setup({
 				snippet = {
 					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
+						luasnip.lsp_expand(args.body)
 					end,
 				},
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" },
+				}),
+				mapping = cmp.mapping.preset.insert({
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<CR>"] = cmp.mapping.confirm {
+						behavior = cmp.ConfirmBehavior.Replace,
+						select = true,
+					},
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, {"i", "s"}),
 				})
 			})
 		end,

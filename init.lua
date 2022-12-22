26989945c3f030171e5bc83edae5e4df59a3e2c0
Joseph Tessitore <joetgamerman3@gmail.com>
2022-12-22 14:09:14 -0500

    did some catppuccin and lsp

--- init.lua
+++ init.lua
@@ -10,6 +10,7 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.runtimepath:prepend(lazypath)
+
 require("lazy").setup({
 	{
 		"nvim-treesitter/nvim-treesitter",
@@ -27,4 +28,37 @@ require("lazy").setup({
 			require("mason").setup()
 		end,
 	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {"williamboman/mason.nvim", "neovim/nvim-lspconfig"},
+		config = function()
+			require("mason-lspconfig").setup()
+
+			require("lspconfig").gopls.setup({})
+		end,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+		},
+		config = function()
+			local cmp = require("cmp")
+			require("mason-lspconfig")
+
+			cmp.setup({
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+				})
+			})
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+	}
 })
+
+vim.cmd("colorscheme catppuccin")
